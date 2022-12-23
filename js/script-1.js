aa83e37e7c9c3fa8a9b72d26d6fde25f9e340f82
S.Palashynskyi <112510462+Palashynskyi@users.noreply.github.com>
2022-12-24 00:48:33 +0200

    Update script-1.js

--- js/script-1.js
+++ js/script-1.js
@@ -1,20 +1,11 @@
-function getDiscount(totalSpent) {
-  const BASE_DISCOUNT = 0;
-  const BRONZE_DISCOUNT = 0.02;
-  const SILVER_DISCOUNT = 0.05;
-  const GOLD_DISCOUNT = 0.1;
-  let discount;
-  // Change code below this line
-if (totalSpent >= 50000) {
-  let discount = 0.1;
-} else if (totalSpent >= 20000) {
-  let discount = 0.05;
-} else if (totalSpent >= 5000) {
-  let discount = 0.02;
-} else {let discount = 0}
-  
-  // Change code above this line
-    return discount;
-    console.log(discount)
+const clients = ["Mango", "Ajax", "Poly"];
+
+for (const client of clients) {
+  console.log(client);
 }
-function getDiscount(137000)
+
+const string = "javascript";
+
+for (const character of string) {
+  console.log(character);
+}
